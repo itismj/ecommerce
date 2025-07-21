006cca38fba8e28994baf4e49261277aef505afd
mhojiboboev14457 <mhojiboboev14457@ada.edu.az>
2025-07-21 08:53:23 +0500

    Initial project files

---
+++ src/test/java/com/mjcoder/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjcoder.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
